6be25c9f5ce63093d1d7a1f18da2dcce2fda2d4a
Leandro Damacena <ld.damacena@gmail.com>
2019-06-12 07:50:17 -0300

    ADD: github auth integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ListRepositoriesComponent } from './components/list-repositories/list-repositories.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListRepositoriesComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent },
+      { path: '', component: ListRepositoriesComponent },
+      { path: '**', redirectTo: '/' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/list-repositories/list-repositories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-repositories',
+  templateUrl: './list-repositories.component.html',
+  styleUrls: ['./list-repositories.component.css']
+})
+export class ListRepositoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
