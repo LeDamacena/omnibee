f50f8cbf3d8573fab512e9a4e5e7b2a4947c4ad6
Leandro Damacena <ld.damacena@gmail.com>
2019-06-17 07:28:27 -0300

    ALTER server port

--- server.js
+++ server.js
@@ -8,4 +8,8 @@ app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname + "/dist/omnibees/index.html"));
 });
 
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log("App is running on port " + port);
+});
