178f8b7863ebed8632e32c185245de48e9a50fff
Leandro de Oliveira Damacena <leandro.damacena@SOUPI-WW.local>
2019-06-18 14:14:51 -0300

    FIX: error in route on Heroku

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.use(express.static(__dirname + "/dist/omnibees"));
 
 const port = process.env.PORT || 8000;
 
+app.get('', function(req, res) {
+    res.sendFile(path.join(__dirname + "/dist/omnibees", 'index.html'));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + "/dist/omnibees", 'index.html'));
+});
+
 app.listen(port, () => {
   console.log("App is running on port " + port);
 });
